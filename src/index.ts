ba239e3b67d26be6029373183bd0e37cf0866373
Ayush Chivate <ayushchivate@gmail.com>
2023-09-02 20:33:32 -0400

    Completed lobby joining and leaving logic

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const MAX_PLAYERS = 5
 type Player = {
     socketId: string
 }
-const players: Player[] = []
+let players: Player[] = []
 
 const io = new Server({
     cors: {
@@ -14,28 +14,36 @@ const io = new Server({
 })
 
 io.on('connection', socket => {
-    players.push({ socketId: socket.id })
-    console.log(players)
-    if (players.length <= MAX_PLAYERS) {
-        socket.join(room)
-        console.log(
-            `Player "${socket.id}" has connected and joined room "${room}".`
-        )
-        if (players.length == 5) {
-            console.log(`${MAX_PLAYERS} players reached.`)
-            io.emit('has-last-player-joined', true)
-        }
-    } else {
+    if (players.length + 1 > MAX_PLAYERS) {
         console.log(
             `Player ${socket.id} unable to join. Maximum of ${MAX_PLAYERS} players reached.`
         )
         socket.emit('is-room-full', true)
+        return
+    }
+
+    socket.join(room)
+    console.log(
+        `Player "${socket.id}" has connected and joined room "${room}".`
+    )
+
+    players.push({ socketId: socket.id })
+    console.log(players)
+
+    if (players.length == 5) {
+        console.log(`Maximum of ${MAX_PLAYERS} players reached.`)
+        io.to(room).emit('disable-start', false)
     }
 
     socket.on('disconnect', () => {
-        players.pop()
+        players = players.filter(player => player.socketId != socket.id)
         console.log(`Player "${socket.id}" has disconnected.`)
         console.log(players)
+        // disable start game button
+        if (players.length < 5) {
+            io.to(room).emit('is-room-full', false)
+            io.to(room).emit('disable-start', true)
+        }
     })
 })
 
